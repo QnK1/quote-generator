53ad6011322536b47072ccfe83190cdf57de68d7
kacper <kacperro03@gmail.com>
2023-08-01 17:24:00 +0200

    finished

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const quoteBtn = document.querySelector('.new-button');
 const twitterBtn = document.querySelector('.twitter-button');
 const quoteText = document.querySelector('.quote');
 const authorText = document.querySelector('.author-text');
+const loader = document.querySelector('.loader');
+const quoteContainer = document.querySelector('.quote-container');
 
 let quotes = [];
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function loaded(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 function pickQuote(){
     const rnd = Math.floor(Math.random() * quotes.length);
@@ -20,20 +32,51 @@ async function getQuotes(){
         const data = await response.json();
 
         quotes = data;
+        changeQuote();
     }catch(err){
         alert(err);
     }
+
+    
 };
 
-//on load
-getQuotes();
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
 
-quoteBtn.addEventListener('click', () => {
+function changeQuote(){
     const result = pickQuote();
 
     const currQuote = result.text;
     const currAuthor = result.author;
 
+    if(currQuote.length > 100){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
     quoteText.innerText = currQuote;
-    authorText.innerText = currAuthor;
+
+    if(currAuthor)
+        authorText.innerText = currAuthor;
+    else
+        authorText.innerText = 'Unknown';
+};
+
+//on load
+loading();
+getQuotes();
+loaded();
+
+
+quoteBtn.addEventListener('click', () => {
+    loading();
+    changeQuote();
+    loaded();
+});
+
+
+twitterBtn.addEventListener('click', () => {
+    tweetQuote();
 });
